b1cacd7ae01dd39cdeeea67514449c30a5361bfd
Scott Thornton <38704968+scthornton@users.noreply.github.com>
2019-07-26 12:12:48 -0400

    Update azure_accept_terms.py
    Commented out versions not included in the deploy.

--- cloud/azure/azure_accept_terms/azure_accept_terms.py
+++ cloud/azure/azure_accept_terms/azure_accept_terms.py
@@ -5,8 +5,8 @@ import sys
 sys.sterr = sys.stdout
 #az vm image accept-terms --urn paloaltonetworks:vmseries1:bundle1:8.1.0
 get_default_cli().invoke(['vm', "image", "accept-terms", "--urn", "paloaltonetworks:vmseries1:bundle1:8.1.0"], out_file=sys.stdout)
-get_default_cli().invoke(['vm', "image", "accept-terms", "--urn", "paloaltonetworks:vmseries1:bundle1:7.1.1"], out_file=sys.stdout)
-get_default_cli().invoke(['vm', "image", "accept-terms", "--urn", "paloaltonetworks:vmseries1:bundle1:8.0.0"], out_file=sys.stdout)
-get_default_cli().invoke(['vm', "image", "accept-terms", "--urn", "paloaltonetworks:vmseries1:bundle1:8.0.17"], out_file=sys.stdout)
-get_default_cli().invoke(['vm', "image", "accept-terms", "--urn", "paloaltonetworks:vmseries1:bundle1:9.0.1"], out_file=sys.stdout)
+# get_default_cli().invoke(['vm', "image", "accept-terms", "--urn", "paloaltonetworks:vmseries1:bundle1:7.1.1"], out_file=sys.stdout)
+# get_default_cli().invoke(['vm', "image", "accept-terms", "--urn", "paloaltonetworks:vmseries1:bundle1:8.0.0"], out_file=sys.stdout)
+# get_default_cli().invoke(['vm', "image", "accept-terms", "--urn", "paloaltonetworks:vmseries1:bundle1:8.0.17"], out_file=sys.stdout)
+# get_default_cli().invoke(['vm', "image", "accept-terms", "--urn", "paloaltonetworks:vmseries1:bundle1:9.0.1"], out_file=sys.stdout)
 
